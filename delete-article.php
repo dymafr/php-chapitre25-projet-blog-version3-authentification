d004a2db5cf865f7baf7e5c87df9c5f7c9031324
Dyma <32801166+dymafr@users.noreply.github.com>
2021-05-18 19:18:14 +0200

    Update delete-article.php

--- delete-article.php
+++ delete-article.php
@@ -4,11 +4,11 @@ $authDB = require_once __DIR__ . '/database/security.php';
 
 $currentUser = $authDB->isLoggedin();
 if ($currentUser) {
-    $articleDB = require_once __DIR__ . './database/models/ArticleDB.php';
+    $articleDB = require_once __DIR__ . '/database/models/ArticleDB.php';
     $_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     $id = $_GET['id'] ?? '';
     if ($id) {
-        $article = $articleDB->fetch($id);
+        $article = $articleDB->fetchOne($id);
         if ($article['author'] === $currentUser['id']) {
             $articleDB->deleteOne($id);
         }
